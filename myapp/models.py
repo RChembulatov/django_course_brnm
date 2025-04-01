63fc16d8eb4f47bd8e7dc31b4eb621d4e4ebab84
RChembulatov <rn.chembulatov@gmail.com>
2025-04-02 01:43:22 +0300

    Добавил модели и внес несколько записей в БД

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField("Название категории", max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField("Название продукта", max_length=100)
+    price = models.DecimalField("Цена", max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name}-{self.price}"
