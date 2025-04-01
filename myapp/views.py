4baebb09be51331b070bf86356adc56de24bb299
RChembulatov <rn.chembulatov@gmail.com>
2025-04-02 02:11:39 +0300

    Добавил шаблон product_list.html и маршрутизацию products

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from myapp.models import Product
 
 
 def home(request):
     return render(request, 'home.html')
+
+
+def product_list(request):
+    product = Product.objects.all()
+    context = dict(products=product)
+
+    return render(request, 'product_list.html', context)
